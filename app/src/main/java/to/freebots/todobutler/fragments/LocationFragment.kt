510de78e42f5e8a71eb472c5d7225aac206d3d03
free-bots <free-bots.to@protonmail.com>
2020-03-28 11:24:31 +0100

    added map to location

--- app/src/main/java/to/freebots/todobutler/fragments/LocationFragment.kt
+++ app/src/main/java/to/freebots/todobutler/fragments/LocationFragment.kt
@@ -1,17 +1,33 @@
 package to.freebots.todobutler.fragments
 
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_location.*
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.overlay.MinimapOverlay
+import org.osmdroid.views.overlay.compass.CompassOverlay
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider
+import org.osmdroid.views.overlay.gestures.RotationGestureOverlay
 import to.freebots.todobutler.R
+
 
 /**
  * A simple [Fragment] subclass.
  */
 class LocationFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Configuration.getInstance().load(context, activity?.getPreferences(Context.MODE_PRIVATE))
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,4 +38,54 @@ class LocationFragment : Fragment() {
     }
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initMap()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        map.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        map.onPause()
+    }
+
+    private fun initMap() {
+        map.setTileSource(TileSourceFactory.MAPNIK)
+        map.setBuiltInZoomControls(true)
+        map.setMultiTouchControls(true)
+
+        val mapController = map.controller
+        mapController.setZoom(9.5)
+        val startPoint = GeoPoint(48.8583, 2.2944)
+        mapController.setCenter(startPoint)
+
+
+        // compass
+        val complass = CompassOverlay(context, InternalCompassOrientationProvider(context), map)
+        complass.enableCompass()
+        map.overlays.add(complass)
+
+
+        // rotation
+        val rotation = RotationGestureOverlay(context, map)
+            .apply {
+                isEnabled = true
+            }
+        map.overlays.add(rotation)
+
+
+        // minimap
+        val minimap = MinimapOverlay(context, map.tileRequestCompleteHandler)
+            .apply {
+                width = map.width / 5
+                height = map.height / 5
+            }
+        map.overlays.add(minimap)
+
+    }
 }
--- app/src/main/java/to/freebots/todobutler/fragments/TaskFragment.kt
+++ app/src/main/java/to/freebots/todobutler/fragments/TaskFragment.kt
@@ -87,7 +87,7 @@ class TaskFragment : Fragment(), TasksAdapter.Action, DatePickerDialog.OnDateSet
                 return true
             }
             R.id.menu_location -> {
-                // TODO show location fragment
+                findNavController().navigate(R.id.action_taskFragment_to_locationFragment)
                 return true
             }
             R.id.menu_subtask -> {
